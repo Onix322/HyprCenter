c3bee2d49ec014a0edf37e077075ebdd5dd7e016
Onix322 <alexandru.dobos3@gmail.com>
2026-02-08 21:04:08 +0100

    UserInterface does not support callback funtion anymore

--- include/UserInterface.hpp
+++ include/UserInterface.hpp
@@ -7,21 +7,17 @@ private:
   std::string name;
   std::string button_name;
   GtkWidget *content;
-  GtkCallbackAction *callback; // functionality
 
   UserInterface();
 
 public:
-  UserInterface(std::string name, std::string button_name, GtkWidget *content,
-                GtkCallbackAction *callback) {
+  UserInterface(std::string name, std::string button_name, GtkWidget *content) {
     this->name = name;
     this->button_name = button_name;
     this->content = content;
-    this->callback = callback;
   };
 
   std::string get_name() { return name; };
   std::string get_button_name() { return button_name; }
   GtkWidget *get_content() { return content; };
-  GtkCallbackAction *get_callbak() { return callback; };
 };
